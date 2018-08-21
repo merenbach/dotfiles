8f5ada8001edc3d0aec45870517731cebd2cdd94
Andrew Merenbach <andrew@merenbach.com>
2018-08-21 08:16:57 -0700

    Update FreeBSD journal

--- home/journal.sh
+++ home/journal.sh
@@ -1,12 +1,45 @@
-/dev/ada0p1     /usr/home       ufs     rw      1       1
-## DEFERRED: service ntpd onefetch
-freebsd-update fetch install
+echo /dev/ada0p1     /usr/home       ufs     rw      1       1 >> /etc/fstab
+
+# ideally copy /etc/make.conf into place before compiling
+
+sysrc clear_tmp_enable="YES"
+sysrc syslogd_flags="-ss"
+sysrc sendmail_enable="NONE"
+sysrc ntpd_enable="YES"
+# onefetch may negate need for this
+#sysrc ntpd_sync_on_start="YES"
+
+# this might not be really necessary on a desktop
+sysrc powerd_enable="YES"
+
+sysrc dumpdev="NO"
+
 portsnap fetch extract
-/usr/ports/x11/nvidia-driver
-/usr/ports/x11/xorg
+freebsd-update fetch install
+
+cd /usr/ports/ports-mgmt/portmaster
+make install clean BATCH=yes
+
+portmaster -G -d --no-confirm security/ca_root_nss
+service ntpd onefetch
+
+portmaster -G -d --no-confirm /usr/ports/x11/nvidia-driver
 sysrc kld_list+="nvidia-modeset"
-/usr/ports/shells/bash
-/usr/ports/devel/git
+
+portmaster -G -d --no-confirm /usr/ports/x11/xorg
+
+portmaster -G -d --no-confirm devel/dbus
+sysrc dbus_enable="YES"
+
+sysrc devd_enable=YES
+
+portmaster -G -d --no-confirm x11-wm/fvwm2
+portmaster -G -d --no-confirm x11/rofi
+portmaster -G -d --no-confirm x11/compton
+
+portmaster -G -d --no-confirm /usr/ports/shells/bash
+portmaster -G -d --no-confirm /usr/ports/devel/git
+
 #nvidia-xconfig --add-argb-glx-visuals --composite --depth=24
 cat <<EOF > /usr/local/etc/X11/xorg.conf.d/driver-nvidia.conf
 Section "Device"
@@ -20,8 +53,3 @@ Section "Device"
     ##Option         "MetaModes" "nvidia-auto-select +0+0 { ForceCompositionPipeline = On }"
 EndSection
 EOF
-#/usr/ports/x11-wm/bspwm
-/usr/ports/ports-mgmt/portmaster
-portmaster -G x11-wm/fvwm2
-portmaster -G x11/rofi
-portmaster -G x11/compton
