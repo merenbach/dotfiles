8e008f92cb5bb7c12c295b46c31ff372ab1affde
Andrew Merenbach <andrew@merenbach.com>
2018-08-21 11:58:11 -0700

    Refactor journal some more

--- home/journal.sh
+++ home/journal.sh
@@ -1,9 +1,25 @@
-echo /dev/ada0p1     /usr/home       ufs     rw      1       1 >> /etc/fstab
+#! /bin/sh
+
+bootstrap() {
+    echo /dev/ada0p1     /usr/home       ufs     rw      1       1 >> /etc/fstab
+    portsnap fetch extract
+    freebsd-update fetch install
+    /usr/bin/make -C /usr/ports/ports-mgmt/portmaster install clean BATCH=yes
+}
+#bootstrap()
 
 # ideally copy /etc/make.conf into place before compiling
 
 install() {
-        /usr/local/sbin/portmaster --no-confirm -B -G -D "$@"
+	/usr/local/sbin/portmaster --no-confirm -B -G -D "$@"
+}
+enablemodule() {
+	/usr/sbin/sysrc kld_list+="$1"
+	/sbin/kldload "$1"
+}
+enableservice() {
+	/usr/sbin/sysrc "$1_enable"="YES"
+	/usr/sbin/service "$1" start
 }
 
 sysrc fsck_y_enable="YES"
@@ -16,49 +32,38 @@ sysrc powerd_enable="YES"
 
 sysrc dumpdev="NO"
 
-portsnap fetch extract
-freebsd-update fetch install
-
-/usr/bin/make -C /usr/ports/ports-mgmt/portmaster install clean BATCH=yes
 
 install security/ca_root_nss
 
-sysrc ntpd_enable="YES"
 # TODO: why do we do this again?
 sysrc ntpd_sync_on_start="YES"
 service ntpd onefetch
-service ntpd start
+enableservice ntpd
 
 install x11/xorg
 
-sysrc kld_list+="coretemp"
-#sysrc kld_list+="amdtemp"
+enablemodule coretemp
+#enablemodule amdtemp
 
 install x11/nvidia-driver
-sysrc kld_list+="nvidia-modeset"
-kldload nvidia-modeset
+enablemodule nvidia-modeset
 
 install devel/dbus
-sysrc dbus_enable="YES"
-service dbus start
+enableservice dbus
 
 install sysutils/smartmontools
 cp -i /usr/local/etc/smartd.conf.sample /usr/local/etc/smartd.conf
-sysrc smartd_enable="YES"
-service smartd start
+enableservice smartd
 
 sysrc firewall_type="workstation"
 sysrc firewall_quiet="YES"
 #sysrc firewall_myservices="ssh"
 #sysrc firewall_allowservices="any"
 sysrc firewall_logdeny="YES"
-sysrc ipfw_enable="YES"
-service ipfw start
+enableservice ipfw
 
 install sysutils/devcpu-data
-sysrc microcode_update_enable="YES"
-# may be redundant since enabling seems to run
-service microcode_update start
+enableservice microcode_update
 
 #TODO: is this necessary?
 #sysrc devd_enable=YES
