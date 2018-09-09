9b3e2b659d167900556c999967ad1d0b4986defd
Andrew Merenbach <andrew@merenbach.com>
2018-09-09 15:56:50 -0700

    Reorganize software install alphabetically

--- home/journal.sh
+++ home/journal.sh
@@ -41,17 +41,22 @@ sysrc dumpdev="NO"
 # this might not be really necessary on a desktop
 enableservice powerd
 
+sysrc firewall_type="workstation"
+sysrc firewall_quiet="YES"
+#sysrc firewall_myservices="ssh"
+#sysrc firewall_allowservices="any"
+sysrc firewall_logdeny="YES"
+enableservice ipfw
+
+enablemodule coretemp
+#enablemodule amdtemp
+
 install "security/ca_root_nss"
 
 # TODO: why do we do this again?
 sysrc ntpd_sync_on_start="YES"
 service ntpd onefetch
 enableservice ntpd
-
-install "x11/xorg"
-
-enablemodule coretemp
-#enablemodule amdtemp
 
 install "x11/nvidia-driver"
 enablemodule nvidia-modeset
@@ -63,24 +68,13 @@ install "sysutils/smartmontools"
 cp -i /usr/local/etc/smartd.conf.sample /usr/local/etc/smartd.conf
 enableservice smartd
 
-sysrc firewall_type="workstation"
-sysrc firewall_quiet="YES"
-#sysrc firewall_myservices="ssh"
-#sysrc firewall_allowservices="any"
-sysrc firewall_logdeny="YES"
-enableservice ipfw
-
 install "sysutils/devcpu-data"
 enableservice microcode_update
 
 #TODO: is this necessary?
 # enableservice devd
 
-install "x11-wm/fvwm2"
-install "x11-wm/compton"
-
 install "shells/bash" "shells/bash-completion"
-
 chsh -s /usr/local/bin/bash "${MY_USER}"
 
 ##nvidia-xconfig --add-argb-glx-visuals --composite --depth=24
@@ -97,78 +91,24 @@ chsh -s /usr/local/bin/bash "${MY_USER}"
 #EndSection
 #EOF
 
-install "x11/rofi"
-install "x11/xlockmore"
-install "x11/xautolock"
-
-
-# utils
-install "archivers/unzip"
+install \
+  "archivers/unzip" \
   "archivers/zip" \
   "devel/awscli" \
-  "ftp/curl" \
-  "ftp/wget" \
-  "graphics/feh" \
-  "graphics/geeqie" \
-  "math/calc" \
-  "net-mgmt/whatmask" \
-  "net/rclone" \
-  "net/rsync" \
-  "security/nmap" \
-  "sysutils/cmdwatch" \
-  "sysutils/lsof" \
-  "sysutils/pwgen" \
-  "textproc/dict" \
-  "textproc/jq" \
-  "textproc/the_silver_searcher" \
-  "x11/arandr"
-
-install "graphics/xpdf" \
-  "security/keepassx2" \
-  "x11-fm/pcmanfm" \
-  "x11/rxvt-unicode" \
-  "x11/urxvt-perls" \
-  "x11/xsel"
-
-install "editors/libreoffice" \
-  "editors/texmaker" \
-  "graphics/gimp" \
-  "multimedia/audacious" \
-  "multimedia/audacious-plugins" \
-  "multimedia/gstreamer-plugins-bad" \
-  "multimedia/gstreamer-plugins-good" \
-  "multimedia/gstreamer-plugins-ugly" \
-  "multimedia/mpv" \
-  "multimedia/smplayer" \
-  "multimedia/vlc" \
-  "print/texlive-full" \
-  "textproc/hs-pandoc" \
-  "www/firefox" \
-  "www/chromium"
-
-install "devel/git" \
-  "editors/vim" \
+  "devel/git" \
+  "devel/hs-cabal-install" \
   "editors/emacs" \
-  "lang/gcc7" \
-  "lang/ghc" \
-  "lang/go" \
-  "lang/python3" \
-  "lang/rust" \
-  "lang/racket-minimal" \
-  "misc/sloccount"
-
-# install "devel/hs-cabal-install"
-# install "devel/stack"
-
-install "emulators/i386-wine-devel" \
+  "editors/libreoffice" \
+  "editors/texmaker" \
+  "editors/vim" \
+  "emulators/dosbox" \
+  "emulators/i386-wine-devel" \
   "emulators/wine-gecko-devel" \
   "emulators/wine-mono-devel" \
-  "emulators/winetricks"
-/bin/sh /usr/local/share/wine/patch-nvidia.sh
-
-install "emulators/dosbox"
-
-install "games/alephone" \
+  "emulators/winetricks" \
+  "ftp/curl" \
+  "ftp/wget" \
+  "games/alephone" \
   "games/alephone-data" \
   "games/alephone-scenarios" \
   "games/angband" \
@@ -177,13 +117,64 @@ install "games/alephone" \
   "games/scummvm-tools" \
   "games/sokoban" \
   "games/xmahjongg" \
-  "games/xsokoban"
-
-install "x11-fonts/dina" \
+  "games/xsokoban" \
+  "graphics/feh" \
+  "graphics/geeqie" \
+  "graphics/gimp" \
+  "graphics/xpdf" \
+  "lang/gcc7" \
+  "lang/ghc" \
+  "lang/go" \
+  "lang/python3" \
+  "lang/racket-minimal" \
+  "lang/rust" \
+  "math/calc" \
+  "misc/sloccount" \
+  "multimedia/audacious" \
+  "multimedia/audacious-plugins" \
+  "multimedia/gstreamer-plugins-bad" \
+  "multimedia/gstreamer-plugins-good" \
+  "multimedia/gstreamer-plugins-ugly" \
+  "multimedia/mpv" \
+  "multimedia/smplayer" \
+  "multimedia/vlc" \
+  "net-mgmt/whatmask" \
+  "net/rclone" \
+  "net/rsync" \
+  "print/texlive-full" \
+  "security/keepassx2" \
+  "security/nmap" \
+  "sysutils/cmdwatch" \
+  "sysutils/lsof" \
+  "sysutils/pwgen" \
+  "textproc/dict" \
+  "textproc/hs-pandoc" \
+  "textproc/jq" \
+  "textproc/the_silver_searcher" \
+  "www/chromium" \
+  "www/firefox" \
+  "x11-fm/pcmanfm" \
+  "x11-fonts/dina" \
   "x11-fonts/droid-fonts-ttf" \
   "x11-fonts/freefont-ttf" \
   "x11-fonts/gnu-unifont" \
   "x11-fonts/terminus-font" \
   "x11-fonts/urwfonts" \
   "x11-fonts/webfonts" \
-  "x11-fonts/xorg-fonts"
+  "x11-fonts/xorg-fonts" \
+  "x11-wm/compton" \
+  "x11-wm/fvwm2" \
+  "x11/arandr" \
+  "x11/rofi" \
+  "x11/rxvt-unicode" \
+  "x11/urxvt-perls" \
+  "x11/xautolock" \
+  "x11/xlockmore" \
+  "x11/xorg" \
+  "x11/xsel"
+
+# needed for i386-wine-devel on Nvidia cards
+/bin/sh /usr/local/share/wine/patch-nvidia.sh
+
+## not currently used
+# install "devel/stack"
