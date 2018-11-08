2ed305172706803c04d314b89eb6a29d2cb3f9e7
Andrew Merenbach <andrew@merenbach.com>
2018-11-08 14:44:28 -0800

    Update journals

---
+++ home/journal-fedora.sh
@@ -0,0 +1,13 @@
+gnome-tweaks
+rclone
+vim-enhanced
+chromium
+sudo dnf install chromium mozilla-ublock-origin
+sudo dnf install mozilla-https-everywhere mozilla-privacy-badger
+sudo dnf install wine.i686
+
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install gstreamer-plugins-bad gstreamer-plugins-good gstreamer-plugins-ugly
+gstreamer1-libav gstreamer1-plugins-bad-free gstreamer1-plugins-bad-nonfree gstreamer1-plugins-good gstreamer1-plugins-ugly
+sudo dnf install playonlinux
+sudo dnf install winetricks
