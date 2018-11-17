63d49a7e8920c4e882052d72eb6df177467fa18f
Andrew Merenbach <andrew@merenbach.com>
2018-11-17 01:14:03 -0800

    Update Fedora journal

--- home/journal-fedora.sh
+++ home/journal-fedora.sh
@@ -16,19 +16,47 @@ dnf update
 # DISABLE CSMT IN WINECFG IN FEDORA
 
 
-gnome-tweaks
-rclone
-vim-enhanced
+
+sudo dnf install awscli
+sudo dnf install calc
+sudo dnf install curl
+sudo dnf install emacs
+sudo dnf install git
+sudo dnf install golang
+sudo dnf install jq
+sudo dnf install nmap
+sudo dnf install pandoc
+sudo dnf install patch
+sudo dnf install pwgen
+sudo dnf install rclone
+sudo dnf install ripgrep
+sudo dnf install rsync
+sudo dnf install rust cargo
+sudo dnf install sloccount
+sudo dnf install texlive
+sudo dnf install vim-commentary
+sudo dnf install vim-enhanced vim-fugitive vim-go
+sudo dnf install wget
+sudo dnf install whatmask
+sudo dnf install zip unzip
+
 sudo dnf install chromium
+sudo dnf install dosbox
+sudo dnf install firefox
+sudo dnf install gimp
+sudo dnf install gnome-tweaks
+sudo dnf install gramps
+sudo dnf install keepassx
 sudo dnf install mozilla-ublock-origin mozilla-https-everywhere mozilla-privacy-badger
+sudo dnf install scummvm
 sudo dnf install wine.i686
+sudo dnf install winetricks
+
 
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install gstreamer-plugins-bad gstreamer-plugins-good gstreamer-plugins-ugly
 gstreamer1-libav gstreamer1-plugins-bad-free gstreamer1-plugins-bad-nonfree gstreamer1-plugins-good gstreamer1-plugins-ugly
 sudo dnf install playonlinux
-sudo dnf install winetricks
-sudo dnf install keepassx
 sudo dnf install gstreamer1-plugins-bad-free-extras gstreamer1-plugins-bad-free-fluidsynth
 
 
